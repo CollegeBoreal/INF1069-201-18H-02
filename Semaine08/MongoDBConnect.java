224efaf63e9b720ae9a64546abf87ee9e51586d7
SeydouSall <34757146+SeydouSall@users.noreply.github.com>
2018-04-12 14:35:00 -0400

    Update MongoDBConnect.java

--- Semaine08/MongoDBConnect.java
+++ Semaine08/MongoDBConnect.java
@@ -6,21 +6,22 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoException;
 
-/*** La classe  MongoDBConnect permet de connecter Java a MongoDB tout en gerant les exceptions ***/
+/*** La classe  MongoDBConnect permet de connecter Java a MongoDB tout en gerant les exceptions
+     et afficher toutes les bases de donnees se trouvant dans le server de mongo ***/
 
 public class MongoDBConnect {
     public static void main(String[] args) {
         MongoClient mongoClient = null;
         try {
-            mongoClient = new MongoClient( "127.0.0.1" , 27017 ); /* localhost a la place de 127.0.0.1 port par defaut de mongoDB*/
+            /*** Connect to a server ***/ 
+            mongoClient = new MongoClient( "127.0.0.1" , 27017 ); /* on peut aussi remplacer 127.0.0.1 par localhost port par defaut de mongoDB*/
  
             System.out.println("Connected to MongoDB!");
             
-            System.out.println("List of databases:");
-
-
+            /*** Print Database names ***/
+            
             for (String databaseName : mongoClient.listDatabaseNames()) {
-
+                System.out.println("List of databases:");
                 System.out.println( databaseName);
 
             }
